041ea752e1d5a6018e5042d8e8fc1e4ba1102fe4
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2025-12-26 15:45:39 +0900

    CORSテスト

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -10,6 +10,16 @@ const nextConfig = {
 			},
 		],
 	},
+	headers: [
+		{
+			source: "/api/:path*",
+			headers: [
+				{ key: "Access-Control-Allow-Origin", value: "*" },
+				{ key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,POST,PUT,DELETE" },
+				{ key: "Access-Control-Allow-Headers", value: "Content-Type" },
+			],
+		},
+	],
 };
 
 export default nextConfig;
